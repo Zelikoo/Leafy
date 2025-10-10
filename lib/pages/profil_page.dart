1eedf9b7b9c03a63b18c6c3dac0b62e2e8ec40ba
Cyril <dcyril95@gmail.com>
2025-10-10 15:39:56 +0200

    refactor: Enhance login page UI and functionality
    This commit refactors the `ProfilPage` to improve its user interface and add state management for a better user experience.
    
    - Converts `ProfilPage` from a `StatelessWidget` to a `StatefulWidget` to manage local state.
    - Wraps the page content in a `SingleChildScrollView` to prevent overflow on smaller screens.
    - Replaces `hintText` with `labelText` for `Email` and `Mot de passe` fields and styles the labels.
    - Implements an `OutlineInputBorder` for the text fields.
    - Adds password visibility toggle functionality to the "Mot de passe" field, allowing users to show or hide the password.
    - Sets appropriate keyboard types (`TextInputType.emailAddress` and `TextInputType.visiblePassword`) and disables autocorrect for the input fields.

--- lib/pages/profil_page.dart
+++ lib/pages/profil_page.dart
@@ -1,7 +1,16 @@
 import 'package:flutter/material.dart';
 
-class ProfilPage extends StatelessWidget {
+class ProfilPage extends StatefulWidget {
   const ProfilPage({super.key});
+
+  @override
+  State<ProfilPage> createState() => _ProfilPageState();
+}
+
+class _ProfilPageState extends State<ProfilPage> {
+  late bool passwordVisible = false;
+  late String email = '';
+  late String password = '';
 
   @override
   Widget build(BuildContext context) {
@@ -9,7 +18,7 @@ class ProfilPage extends StatelessWidget {
       body: Container(
         width: double.infinity,
         height: double.infinity,
-        padding: const EdgeInsets.all(50),
+        //padding: const EdgeInsets.all(50),
         decoration: const BoxDecoration(
           gradient: LinearGradient(
             begin: Alignment.topCenter,
@@ -18,146 +27,202 @@ class ProfilPage extends StatelessWidget {
           ),
         ),
 
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-
-          children: [
-            Image(
-              image: AssetImage("assets/images/leafy.png"),
-              width: 100,
-              height: 100,
-            ),
-            SizedBox(height: 50),
-            TextField(
-              decoration: InputDecoration(
-                hintText: "Email",
-                hintStyle: TextStyle(color: Colors.black),
-              ),
-            ),
-            SizedBox(height: 10),
-            TextField(
-              decoration: InputDecoration(
-                hintText: "Mot de passe",
-                hintStyle: TextStyle(color: Colors.black),
-              ),
-            ),
-            SizedBox(height: 10),
-            Align(
-              alignment: Alignment.centerRight,
-              child: TextButton(
-                onPressed: () {
-                  print('mot de passe oublié');
-                },
-                child: Text("Mot de passe oublié ?"),
-              ),
-            ),
-            SizedBox(height: 10),
-            Container(
-              width: double.infinity,
-              child: TextButton(
-                onPressed: () {
-                  print('connexion');
-                },
-
-                style: TextButton.styleFrom(
-                  padding: EdgeInsets.symmetric(horizontal: 50, vertical: 10),
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(25),
-                    side: BorderSide(color: Colors.black),
-                  ),
-                ),
-                child: Text(
-                  "Connexion",
-                  style: TextStyle(
-                    color: Colors.black,
-                    fontSize: 20,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-              ),
-            ),
-            SizedBox(height: 20),
-            Container(
-              width: double.infinity,
-              child: TextButton(
-                onPressed: () {
-                  print('inscription');
-                },
-                style: TextButton.styleFrom(
-                  padding: EdgeInsets.symmetric(horizontal: 50, vertical: 10),
-                  backgroundColor: Colors.lightBlue.shade800,
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(25),
-                  ),
-                ),
-                child: Text(
-                  "S'inscrire",
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 20,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-              ),
-            ),
-            SizedBox(height: 20),
-            Text(
-              "Ou connectez-vous avec",
-              style: TextStyle(color: Colors.black),
-            ),
-            SizedBox(height: 20),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        child: Center(
+          child: SingleChildScrollView(
+            physics: BouncingScrollPhysics(),
+            padding: EdgeInsets.all(50),
+            child: Column(
+              // mainAxisAlignment: MainAxisAlignment.center,
+              // crossAxisAlignment: CrossAxisAlignment.center,
               children: [
-                TextButton(
-                  onPressed: () {
-                    print('connexion facebook');
-                  },
-                  style: TextButton.styleFrom(
-                    padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-                    backgroundColor: Colors.blue,
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(30),
+                Image(
+                  image: AssetImage("assets/images/leafy.png"),
+                  width: 100,
+                  height: 100,
+                ),
+                SizedBox(height: 50),
+                TextField(
+                  decoration: InputDecoration(
+                    labelText: "Email",
+                    labelStyle: TextStyle(color: Colors.black, fontSize: 20),
+
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10),
                     ),
                   ),
-                  child: Row(
-                    children: [
-                      Icon(Icons.facebook, color: Colors.white, size: 25),
-                      SizedBox(width: 10),
-                      Text(
-                        "Facebook",
-                        style: TextStyle(color: Colors.white, fontSize: 15),
+                  keyboardType: TextInputType.emailAddress,
+                  autocorrect: false,
+                  textCapitalization: TextCapitalization.none,
+                  style: TextStyle(color: Colors.black, fontSize: 20),
+                  onChanged: (value) {
+                    email = value;
+                  },
+                ),
+                SizedBox(height: 20),
+                TextField(
+                  decoration: InputDecoration(
+                    labelText: "Mot de passe",
+                    labelStyle: TextStyle(color: Colors.black, fontSize: 20),
+                    suffixIcon: IconButton(
+                      icon: Icon(
+                        passwordVisible
+                            ? Icons.visibility
+                            : Icons.visibility_off,
+                        color: Colors.black,
                       ),
-                    ],
+                      onPressed: () {
+                        setState(() {
+                          passwordVisible = !passwordVisible;
+                        });
+                      },
+                    ),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                  ),
+                  onChanged: (value) {
+                    password = value;
+                  },
+                  keyboardType: TextInputType.visiblePassword,
+                  obscureText: !passwordVisible,
+                  autocorrect: false,
+                  textCapitalization: TextCapitalization.none,
+                  style: TextStyle(color: Colors.black, fontSize: 20),
+                ),
+                SizedBox(height: 10),
+                Align(
+                  alignment: Alignment.centerRight,
+                  child: TextButton(
+                    onPressed: () {
+                      print('mot de passe oublié');
+                    },
+                    child: Text("Mot de passe oublié ?"),
                   ),
                 ),
-                SizedBox(width: 20),
-                TextButton(
-                  onPressed: () {
-                    print('connexion google');
-                  },
-                  style: TextButton.styleFrom(
-                    padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-                    backgroundColor: Colors.red,
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(30),
+                SizedBox(height: 10),
+                Container(
+                  width: double.infinity,
+                  child: TextButton(
+                    onPressed: () {
+                      print('connexion');
+                    },
+
+                    style: TextButton.styleFrom(
+                      padding: EdgeInsets.symmetric(
+                        horizontal: 50,
+                        vertical: 10,
+                      ),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(25),
+                        side: BorderSide(color: Colors.black),
+                      ),
+                    ),
+                    child: Text(
+                      "Connexion",
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                      ),
                     ),
                   ),
-                  child: Row(
-                    children: [
-                      Icon(Icons.g_mobiledata, color: Colors.white, size: 25),
-                      SizedBox(width: 10),
-                      Text(
-                        "Google",
-                        style: TextStyle(color: Colors.white, fontSize: 15),
+                ),
+                SizedBox(height: 20),
+                Container(
+                  width: double.infinity,
+                  child: TextButton(
+                    onPressed: () {
+                      print('inscription');
+                    },
+                    style: TextButton.styleFrom(
+                      padding: EdgeInsets.symmetric(
+                        horizontal: 50,
+                        vertical: 10,
                       ),
-                    ],
+                      backgroundColor: Colors.lightBlue.shade800,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(25),
+                      ),
+                    ),
+                    child: Text(
+                      "S'inscrire",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
                   ),
+                ),
+                SizedBox(height: 20),
+                Text(
+                  "Ou connectez-vous avec",
+                  style: TextStyle(color: Colors.black),
+                ),
+                SizedBox(height: 20),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    TextButton(
+                      onPressed: () {
+                        print('connexion facebook');
+                      },
+                      style: TextButton.styleFrom(
+                        padding: EdgeInsets.symmetric(
+                          horizontal: 20,
+                          vertical: 10,
+                        ),
+                        backgroundColor: Colors.blue,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(30),
+                        ),
+                      ),
+                      child: Row(
+                        children: [
+                          Icon(Icons.facebook, color: Colors.white, size: 25),
+                          SizedBox(width: 10),
+                          Text(
+                            "Facebook",
+                            style: TextStyle(color: Colors.white, fontSize: 15),
+                          ),
+                        ],
+                      ),
+                    ),
+                    SizedBox(width: 20),
+                    TextButton(
+                      onPressed: () {
+                        print('connexion google');
+                      },
+                      style: TextButton.styleFrom(
+                        padding: EdgeInsets.symmetric(
+                          horizontal: 20,
+                          vertical: 10,
+                        ),
+                        backgroundColor: Colors.red,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(30),
+                        ),
+                      ),
+                      child: Row(
+                        children: [
+                          Icon(
+                            Icons.g_mobiledata,
+                            color: Colors.white,
+                            size: 25,
+                          ),
+                          SizedBox(width: 10),
+                          Text(
+                            "Google",
+                            style: TextStyle(color: Colors.white, fontSize: 15),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
                 ),
               ],
             ),
-          ],
+          ),
         ),
       ),
     );
